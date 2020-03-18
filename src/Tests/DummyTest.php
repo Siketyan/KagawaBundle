27e1b4b71b7605d92f9bf439f4a13867cb18cc0f
Naoki Ikeguchi <root@siketyan.dev>
2020-03-18 14:06:20 +0900

    ✅ Add dummy test

---
+++ src/Tests/DummyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siketyan\KagawaBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true, 'Yay!');
+    }
+}
